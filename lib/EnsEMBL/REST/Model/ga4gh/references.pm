c0d932c6a0a5216517d9abd30a9a1baa031d70a6
Sarah Hunt <seh@ebi.ac.uk>
2015-06-29 16:14:18 +0000

    initial references endpoints

---
+++ lib/EnsEMBL/REST/Model/ga4gh/references.pm
@@ -0,0 +1,151 @@
+=head1 LICENSE
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+=cut
+
+package EnsEMBL::REST::Model::ga4gh::references;
+
+use Moose;
+extends 'Catalyst::Model';
+use Data::Dumper;
+with 'Catalyst::Component::InstancePerContext';
+
+has 'context' => (is => 'ro');
+
+our $species = 'homo_sapiens';
+
+sub build_per_context_instance {
+  my ($self, $c, @args) = @_;
+  return $self->new({ context => $c, %$self, @args });
+}
+
+
+
+sub fetch_references {
+  
+  my $self = shift;
+
+  my $c = $self->context();
+  
+  my $post_data = $c->req->data;
+
+  $c->log->debug(Dumper $post_data);
+
+  $c->go( 'ReturnError', 'custom', [ ' Error - search by md5sum not currently supported'])
+    if exists $post_data->{md5checksums} ;
+
+  my %req;
+  if (exists $post_data->{accessions}){
+    foreach my $acc (@{$post_data->{accessions}}){
+      $req{$acc}++;
+    }
+  }
+  if (exists $post_data->{referenceNames}){
+    foreach my $name (@{$post_data->{referenceNames}}){
+      $req{$name}++;
+    }
+  }
+
+  return $self->getReferences(\%req);
+
+}
+
+
+
+sub getReferences{
+
+  my $self = shift;
+  my $req  = shift;
+
+  my @references;
+  my $count = 0;
+  my $nextToken;
+  my $start = 0;
+
+  my $c = $self->context();
+
+  my $slice_ad = $c->model('Registry')->get_adaptor($species, 'Core', 'slice');
+
+  my @slices = @{$slice_ad->fetch_all('toplevel')};
+
+  foreach my $slice(@slices){
+
+    my $name   = $slice->seq_region_name();   
+    my $tmp_id = $slice->name();
+ 
+    next if (scalar (keys %$req)) > 0  && ! $req->{$name} && ! $req->{$tmp_id};
+ 
+    ## rough paging - is order reliable?
+    $start = 1 if defined $c->req->data->{pageToken} && $tmp_id eq $c->req->data->{pageToken};
+    next if defined $c->req->data->{pageToken} && $start ==0;
+
+    $count++;
+    if (defined $c->req->data->{pageSize} && $count > $c->req->data->{pageSize}){
+
+      $nextToken = $tmp_id;
+      last;
+    }
+   
+    my $ref = $self->format_slice($slice);
+    push @references,  $ref;
+  }
+
+  return { references => \@references, nextPageToken => $nextToken };
+
+}
+
+
+### get single reference by 'id'
+
+sub getReference{
+
+  my ($self,  $get_id ) = @_; 
+
+  my $c = $self->context();
+
+  my $slice_ad = $c->model('Registry')->get_adaptor($species, 'Core', 'slice');
+
+  my $slice = $slice_ad->fetch_by_name($get_id);
+
+  ## exit if not current
+  $self->context()->go( 'ReturnError', 'custom', [ " No data available for this reference : $get_id" ] )
+    unless defined $slice;
+
+  my $reference = $self->format_slice($slice);
+  return { references => [$reference]};
+
+}
+
+sub format_slice{
+
+  my $self  = shift;
+  my $slice = shift;
+
+  my %ref;
+
+  $ref{start}  = 0; ##FIX
+  $ref{length} = $slice->length();
+  $ref{id}     = $slice->display_id();
+  $ref{name}   = $slice->seq_region_name();
+ 
+  my @alternative_names = @{$slice->get_all_synonyms('RefSeq_genomic')} ;
+  $ref{accession}  =  $alternative_names[0]->name() if defined $alternative_names[0];
+
+  warn "Error- 2 accessions\n" if defined $alternative_names[1];
+
+  return \%ref;
+
+}
+
+
+
+
+1;
